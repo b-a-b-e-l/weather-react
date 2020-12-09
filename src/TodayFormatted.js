b6fb9cbc47f6f23a3a810d5d677244e8e1361082
b-a-b-e-l <belenbednarski@gmail.com>
2020-12-09 20:59:41 +0100

    fixing API time date

--- src/TodayFormatted.js
+++ src/TodayFormatted.js
@@ -1,5 +1,13 @@
 export default function TodayFormatted(props) {
-  //console.log(props.date);
+  console.log(props.date);
+  let day = props.date.getDay();
+  let hours = props.date.getHours();
+  let minutes = props.date.getMinutes();
 
-  return "Sunday 03:00";
+  return (
+    <div>
+      {" "}
+      {day} {hours}:{minutes}{" "}
+    </div>
+  );
 }

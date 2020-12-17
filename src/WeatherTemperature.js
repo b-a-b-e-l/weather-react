ce461275eb49692348f2fb74e3828ce7e4f9f5b6
b-a-b-e-l <belenbednarski@gmail.com>
2020-12-17 15:26:33 +0100

    took out the unit converter (to fahrenheit) + styled footer links
    I don't want to have the option of converting the main temperature, if I cant change the units in the wind speed and forecast temperature

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,45 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <h2 className="temp-now">
-        {Math.round(props.celsius)}
-        <span className="celcius">
-          <strong> °C|</strong>
-        </span>
-        <span className="fahrenheit-link">
-          <a href="/" onClick={showFahrenheit}>
-            °F
-          </a>
-        </span>
-      </h2>
-    );
-  } else {
-    let fahrenheit = (props.celsius * 9) / 5 + 32;
-    return (
-      <h2 className="temp-now">
-        {Math.round(fahrenheit)}
-        <span className="fahrenheit">
-          <strong> °F|</strong>
-        </span>
-        <span className="celcius-link">
-          <a href="/" onClick={showCelsius}>
-            °C
-          </a>
-        </span>
-      </h2>
-    );
-  }
+  return (
+    <h2 className="temp-now">
+      {Math.round(props.celsius)}
+      <span className="celcius">°C</span>
+    </h2>
+  );
 }
